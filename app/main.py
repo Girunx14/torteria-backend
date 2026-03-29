7d987d6e6300d456cdb07e0f9f4dcd73f8140284
Girunx14 <navarrogerardo1409@gmail.com>
2026-03-29 14:08:54 -0600

    Fix: Agregar Vercel a CORS

--- app/main.py
+++ app/main.py
@@ -30,7 +30,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "https://torteria-frontend.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
